1ea96519c3fe84abaef9606ac2f9d0ec5edf7f96
Sushil Rahatole <sushilrahatole@gmail.com>
2025-04-26 17:01:36 +0530

    Implemented interview page UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { isAuthenticated } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
